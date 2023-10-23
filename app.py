170961905a93ac87c7dd78c50d68af3a620b84ba
ThiagoBelchior99 <144141841+ThiagoBelchior99@users.noreply.github.com>
2023-10-23 11:53:03 +0000

    Atualizando tudo

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+
+st.set_page_config('CARLÂO - ECOP06','https://unifei.edu.br/wp-content/themes/twentytwelve-child/img/cabecalho/logo-unifei-oficial.png',)
+
+st.title('Página Demo ECOP06')
+
+esportes = pd.read_csv('https://github.com/MainakRepositor/Datasets/raw/master/GeneralEsportData.csv')
+
+st.dataframe(esportes)
